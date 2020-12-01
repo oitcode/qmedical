6db290bb8dda4f432eeeda4231c487c822d9d199
Shyam Sitaula <shyamsitaula@oit.com.np>
2020-12-01 12:11:59 +0545

    falsh msg bug fix

--- resources/views/livewire/expense-component.blade.php
+++ resources/views/livewire/expense-component.blade.php
@@ -1,12 +1,19 @@
 <div>
-  @if (session()->has('message'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-      {{ session('message') }}
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true" class="text-white">&times;</span>
-      </button>
+
+  <!-- Flash message row -->
+  <!-- TODO: Manage this flash message -->
+  <div class="row">
+    <div class="col-md-6">
+      @if (session()->has('message'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+          {{ session('message') }}
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true" class="text-white">&times;</span>
+          </button>
+        </div>
+      @endif
     </div>
-  @endif
+  </div>
 
   <div class="row">
     <div class="col-md-8">
