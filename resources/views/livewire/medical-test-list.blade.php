480d9c6e1e2f6571dfe71bc4aa0d440d68ad6ee4
Shyam Sitaula <shyamsitaula@oit.com.np>
2021-01-10 17:35:05 +0545

    misc

--- resources/views/livewire/medical-test-list.blade.php
+++ resources/views/livewire/medical-test-list.blade.php
@@ -3,8 +3,8 @@
 
     <div wire:loading class="p-2 text-info">
       Loading ...
-      <div class="spinner-grow spinner-grow-sm" role="status">
       {{--
+      <div class="spinner-grow spinner-grow-sm" role="status">
         <span class="sr-only">Loading...</span>
        </div>
       --}}
