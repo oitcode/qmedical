9e042d3bf7193f17944d05383acca55e46d599f0
Shyam Sitaula <shyamsitaula@oit.com.np>
2021-01-10 12:19:35 +0545

    misc

--- resources/views/livewire/medical-test-create-component.blade.php
+++ resources/views/livewire/medical-test-create-component.blade.php
@@ -1,5 +1,5 @@
 <div  wire:ignore.self class="modal" id="createModal" data-backdrop="static" tabindex="-1">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-lg">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title">New Medical Test</h5>
@@ -13,205 +13,216 @@
 
         <form>
 
-          <!-- Date and Test type -->
-          <div class="form-group form-inline m-0">
-            <div class="input-group w-100">
-              <div class="input-group-prepend w-25">
-                <div class="input-group-text w-100">
-                  Date
-                </div>
-              </div>
-
-              <input type="date" class="form-control" id="" placeholder="Date" wire:model.defer="medicalTestDate">
-              @error('medicalTestDate')
-                  <div class="text-danger">{{ $message }}</div>
-              @enderror
-            </div>
-          </div>
-
-          <div class="form-group form-inline m-0">
-            <div class="input-group w-100">
-              <div class="input-group-prepend w-25">
-                <div class="input-group-text w-100">
-                  Test Type
-                </div>
-              </div>
-
-              <select class="custom-select" wire:model.defer="medicalTestTypeId">
-                <option>---</option>
-                 @foreach($medicalTestTypes as $medicalTestType)
-                   <option value="{{ $medicalTestType->medical_test_type_id }}">{{ $medicalTestType->name }}</option>
-                 @endforeach
-              </select>
-              @error('medicalTestTypeId')
-                  <div class="text-danger">{{ $message }}</div>
-              @enderror
-            </div>
-          </div>
-          <!-- /.Date and Test type -->
-
-
-          @include('partials.sleek-input', ['mName' => 'patientName', 'pName' => 'Patient Name', 'faIcon' => 'fas fa-user',])
-
-
-          <!-- Biometric Detail -->
-          <div class="form-group form-inline m-0">
-            <div class="input-group w-100">
-              <div class="input-group-prepend w-25">
-                <div class="input-group-text w-100">
-                  <i class="fas fa-venus-double"></i>
-                </div>
-              </div>
-
-              <select class="custom-select" wire:model.defer="patientSex">
-                <option>Sex</option>
-                  <option value="m">Male</option>
-                  <option value="f">Female</option>
-              </select>
-              @error('patientSex')
-                  <div class="text-danger">{{ $message }}</div>
-              @enderror
-            </div>
-          </div>
-
-          <div class="form-group form-inline m-0">
-            <div class="input-group w-100">
-              <div class="input-group-prepend w-25">
-                <div class="input-group-text w-100">
-                  Date of Birth
-                </div>
-              </div>
-
-              <input type="date" class="form-control" id="" placeholder="Date of birth" wire:model.defer="patientDob">
-              @error('patientDob')
-                  <div class="text-danger">{{ $message }}</div>
-              @enderror
-            </div>
-          </div>
-          <!-- /.Biometric Detail -->
-
-
-          <!-- Contact Detail -->
-          @include('partials.sleek-input', ['mName' => 'patientAddress', 'pName' => 'Address', 'faIcon' => 'fas fa-map-marker-alt',])
-          @include('partials.sleek-input', ['mName' => 'patientContactNumber', 'pName' => 'Phone', 'faIcon' => 'fas fa-phone',])
-          @include('partials.sleek-input', ['mName' => 'patientEmail', 'pName' => 'Email','faIcon' => 'fas fa-envelope', ])
-
-          @include('partials.sleek-input', ['mName' => 'patientPassportNumber', 'pName' => 'Passport Number', 'faIcon' => 'fas fa-passport',])
-          @include('partials.sleek-input', ['mName' => 'patientPassportExpiryDate', 'pName' => 'Passport Expiry Date', 'faIcon' => 'fas fa-calendar',])
-          @include('partials.sleek-input', ['mName' => 'patientPassportIssuePlace', 'pName' => 'Passport Issued Place','faIcon' => 'fas fa-map-marker-alt', ])
-          @include('partials.sleek-input', ['mName' => 'patientNationality', 'pName' => 'Nationality', 'faIcon' => 'fas fa-flag', 'mValue' => 'Nepalese',])
-          <!-- /.Contact Detail -->
-
-
-
-          <!-- Billing -->
-          @include('partials.sleek-input', ['mName' => 'price', 'pName' => 'Price',])
-
-          <!-- Agent -->
-          <div class="form-group form-inline m-0">
-            <div class="input-group w-100">
-              <div class="input-group-prepend w-25">
-                <div class="input-group-text w-100">
-                  Agent ?
-                </div>
-              </div>
-
-              <select class="custom-select" wire:model="agentFlag">
-                <option>---</option>
-                <option>Yes</option>
-                <option>No</option>
-              </select>
-              @error('agentFlag')
-                <div class="text-danger">{{ $message }}</div>
-              @enderror
-            </div>
-          </div>
-
-          @if (strtolower($agentFlag) === 'yes')
-            <div class="form-group form-inline m-0">
-              <div class="input-group w-100">
-                <div class="input-group-prepend w-25">
-                  <div class="input-group-text w-100">
-                    Paid By
+          <div class="form-row">
+            <div class="col-md-8">
+              <!-- Date and Test type -->
+              <div class="form-group form-inline m-0">
+                <div class="input-group w-100">
+                  <div class="input-group-prepend w-25">
+                    <div class="input-group-text w-100">
+                      Date
+                    </div>
                   </div>
+
+                  <input type="date" class="form-control" id="" placeholder="Date" wire:model.defer="medicalTestDate">
+                  @error('medicalTestDate')
+                      <div class="text-danger">{{ $message }}</div>
+                  @enderror
                 </div>
-
-                <select class="custom-select" wire:model="payBy">
-                  <option>---</option>
-                    <option>Agent</option>
-                    <option value="Self">Patient</option>
-                </select>
-                @error('payBy')
-                  <div class="text-danger">{{ $message }}</div>
-                @enderror
               </div>
-            </div>
 
-
-            <div class="form-group form-inline m-0">
-              <div class="input-group w-100">
-                <div class="input-group-prepend w-25">
-                  <div class="input-group-text w-100">
-                    Agent
+              <div class="form-group form-inline m-0">
+                <div class="input-group w-100">
+                  <div class="input-group-prepend w-25">
+                    <div class="input-group-text w-100">
+                      Test Type
+                    </div>
                   </div>
+
+                  <select class="custom-select" wire:model.defer="medicalTestTypeId">
+                    <option>---</option>
+                     @foreach($medicalTestTypes as $medicalTestType)
+                       <option value="{{ $medicalTestType->medical_test_type_id }}">{{ $medicalTestType->name }}</option>
+                     @endforeach
+                  </select>
+                  @error('medicalTestTypeId')
+                      <div class="text-danger">{{ $message }}</div>
+                  @enderror
                 </div>
-
-                <select class="custom-select" wire:model="selectedAgentId" wire:change="selectAgent">
-                  <option>---</option>
-                  @if(count($agents) > 0)
-                    @foreach($agents as $agent)
-                      <option value="{{ $agent->agent_id }}">{{ $agent->name }}</option>
-                    @endforeach
-                  @endif
-                </select>
-                @error('selectedAgentId')
-                    <div class="text-danger">{{ $message }}</div>
-                @enderror
               </div>
-            </div>
+              <!-- /.Date and Test type -->
 
-            @if($selectedAgent && false)
-              <div class="mb-3">
-              <h2 class="lead"><b>{{ $selectedAgent->name }}</b></h2>
-                <ul class="ml-4 mb-0 fa-ul text-muted">
-                  <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> {{ $selectedAgent->contact_number }}</li>
-                </ul>
-              </div>
-            @endif
+              <!-- Biometric Detail -->
+              @include('partials.sleek-input', ['mName' => 'patientName', 'pName' => 'Patient Name', 'faIcon' => 'fas fa-user',])
 
-            @include('partials.sleek-input', ['mName' => 'agentCommission', 'pName' => 'Agent Commission', 'faIcon' => 'fas fa-rupee-sign',])
-
-          @endif
-
-
-
-          <!-- Credit or not -->
-          @if (false)
-          {{-- TODO
-          @if (strtolower($agentFlag) !== 'yes'
-            ||
-            (strtolower($agentFlag) === 'yes' && strtolower($payBy) === 'self')
-
-          )
-          --}}
-            <div class="form-group form-inline m-0">
-              <div class="input-group w-100">
-                <div class="input-group-prepend w-25">
-                  <div class="input-group-text w-100">
-                    Credit
+              <div class="form-group form-inline m-0">
+                <div class="input-group w-100">
+                  <div class="input-group-prepend w-25">
+                    <div class="input-group-text w-100">
+                      <i class="fas fa-venus-double"></i>
+                    </div>
                   </div>
+
+                  <select class="custom-select" wire:model.defer="patientSex">
+                    <option>Sex</option>
+                      <option value="m">Male</option>
+                      <option value="f">Female</option>
+                  </select>
+                  @error('patientSex')
+                      <div class="text-danger">{{ $message }}</div>
+                  @enderror
                 </div>
-                <select class="custom-select" wire:model="creditFlag">
-                  <option>---</option>
-                    <option>No</option>
+              </div>
+
+              <div class="form-group form-inline m-0">
+                <div class="input-group w-100">
+                  <div class="input-group-prepend w-25">
+                    <div class="input-group-text w-100">
+                      Date of Birth
+                    </div>
+                  </div>
+
+                  <input type="date" class="form-control" id="" placeholder="Date of birth" wire:model.defer="patientDob">
+                  @error('patientDob')
+                      <div class="text-danger">{{ $message }}</div>
+                  @enderror
+                </div>
+              </div>
+              <!-- /.Biometric Detail -->
+
+
+              <!-- Contact Detail -->
+              @include('partials.sleek-input', ['mName' => 'patientAddress', 'pName' => 'Address', 'faIcon' => 'fas fa-map-marker-alt',])
+              @include('partials.sleek-input', ['mName' => 'patientContactNumber', 'pName' => 'Phone', 'faIcon' => 'fas fa-phone',])
+              @include('partials.sleek-input', ['mName' => 'patientEmail', 'pName' => 'Email','faIcon' => 'fas fa-envelope', ])
+
+              @include('partials.sleek-input', ['mName' => 'patientPassportNumber', 'pName' => 'Passport Number', 'faIcon' => 'fas fa-passport',])
+              @include('partials.sleek-input', ['mName' => 'patientPassportExpiryDate', 'pName' => 'Passport Expiry Date', 'faIcon' => 'fas fa-calendar',])
+              @include('partials.sleek-input', ['mName' => 'patientPassportIssuePlace', 'pName' => 'Passport Issued Place','faIcon' => 'fas fa-map-marker-alt', ])
+              @include('partials.sleek-input', ['mName' => 'patientNationality', 'pName' => 'Nationality', 'faIcon' => 'fas fa-flag', 'mValue' => 'Nepalese',])
+              <!-- /.Contact Detail -->
+            </div>
+            <div class="col-md-4">
+              <!-- Billing -->
+              <div class="p-2">
+                <h2 class="h5">Billing</h2>
+              </div>
+              @include('partials.sleek-input', ['mName' => 'price', 'pName' => 'Price',])
+
+              <!-- Agent -->
+              <div class="form-group form-inline m-0">
+                <div class="input-group w-100">
+                  <div class="input-group-prepend w-25">
+                    <div class="input-group-text w-100">
+                      Agent ?
+                    </div>
+                  </div>
+
+                  <select class="custom-select" wire:model="agentFlag">
+                    <option>---</option>
                     <option>Yes</option>
-                </select>
-                @error('creditFlag')
+                    <option>No</option>
+                  </select>
+                  @error('agentFlag')
                     <div class="text-danger">{{ $message }}</div>
-                @enderror
+                  @enderror
+                </div>
               </div>
+
+              @if (strtolower($agentFlag) === 'yes')
+                <div class="form-group form-inline m-0">
+                  <div class="input-group w-100">
+                    <div class="input-group-prepend w-25">
+                      <div class="input-group-text w-100">
+                        Paid By
+                      </div>
+                    </div>
+
+                    <select class="custom-select" wire:model="payBy">
+                      <option>---</option>
+                        <option>Agent</option>
+                        <option value="Self">Patient</option>
+                    </select>
+                    @error('payBy')
+                      <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                  </div>
+                </div>
+
+
+                <div class="form-group form-inline m-0">
+                  <div class="input-group w-100">
+                    <div class="input-group-prepend w-25">
+                      <div class="input-group-text w-100">
+                        Agent
+                      </div>
+                    </div>
+
+                    <select class="custom-select" wire:model="selectedAgentId" wire:change="selectAgent">
+                      <option>---</option>
+                      @if(count($agents) > 0)
+                        @foreach($agents as $agent)
+                          <option value="{{ $agent->agent_id }}">{{ $agent->name }}</option>
+                        @endforeach
+                      @endif
+                    </select>
+                    @error('selectedAgentId')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                  </div>
+                </div>
+
+                @if($selectedAgent && false)
+                  <div class="mb-3">
+                  <h2 class="lead"><b>{{ $selectedAgent->name }}</b></h2>
+                    <ul class="ml-4 mb-0 fa-ul text-muted">
+                      <li class="small"><span class="fa-li"><i class="fas fa-lg fa-phone"></i></span> {{ $selectedAgent->contact_number }}</li>
+                    </ul>
+                  </div>
+                @endif
+
+                @include('partials.sleek-input', ['mName' => 'agentCommission', 'pName' => 'Agent Commission', 'faIcon' => 'fas fa-rupee-sign',])
+
+              @endif
+
+
+
+              <!-- Credit or not -->
+              @if (false)
+              {{-- TODO
+              @if (strtolower($agentFlag) !== 'yes'
+                ||
+                (strtolower($agentFlag) === 'yes' && strtolower($payBy) === 'self')
+
+              )
+              --}}
+                <div class="form-group form-inline m-0">
+                  <div class="input-group w-100">
+                    <div class="input-group-prepend w-25">
+                      <div class="input-group-text w-100">
+                        Credit
+                      </div>
+                    </div>
+                    <select class="custom-select" wire:model="creditFlag">
+                      <option>---</option>
+                        <option>No</option>
+                        <option>Yes</option>
+                    </select>
+                    @error('creditFlag')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                  </div>
+                </div>
+              @endif
             </div>
-          @endif
+          </div>
+
+
+
+
+
+
+
 
 
           <div class="mx-2 my-4">
